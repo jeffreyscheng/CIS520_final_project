a355c3a961b586607403487608fe18e28e9aa463
Jeffrey Cheng <jeffch@wharton.upenn.edu>
2017-11-28 13:00:39 -0500

    attempted ensemble model

--- test_script.m
+++ test_script.m
@@ -11,6 +11,8 @@ validationX = X_train_bag(valInd,:);
 validationY = Y_train(valInd,:);
 
 predictions = predict_labels_jank(trainingX, trainingY, validationX);
+validationY(1:10,:)
+predictions(1:10,:)
 score = performance_measure(predictions, validationY)
 e = cputime - t
 function features = sparse_PCA(X)
@@ -25,7 +27,7 @@ function features = sparse_PCA(X)
 
 % 0.1, 0.1, 1000 -> 1.6306
 % 0.3, 0.1, 500 -> 1.7690
-    [U,S,V] = svds(X, 100);
+    [U,S,V] = svds(X, 1000);
     features = U * S;
 end
 
@@ -35,13 +37,16 @@ function [Y_hat] = predict_labels_jank(training_bag, training_labels, X_test_bag
     % nonsparse_training(1:5)
     reduced_train_bag = full(sparse_PCA(training_bag))
     % reduced_train_bag = reduced_train_bag(:,1:1000)
-    cost = [0,3,1,2,3;4,0,2,3,2;1,2,0,2,1;2,1,2,0,2;2,2,2,1,0];
-    SVM_model = fitcecoc(reduced_train_bag,training_labels,'Cost',cost);
-    % SVM_model.costs = cost;
+    ClassNames = {'1', '2', '3', '4', '5'};
+    training_labels = ClassNames(training_labels);
+    cost.ClassNames = ClassNames;
+    cost.ClassificationCosts = [0 3 1 2 3; 4 0 2 3 2; 1 2 0 2 1; 2 1 2 0 2; 2 2 2 1 0];
+    ensemble_model = fitensemble(reduced_train_bag,training_labels,'AdaBoostM2',10,'tree','Cost',cost);
+%     SVM_model = fitensemble(reduced_train_bag,training_labels,'AdaBoostM2',10,'tree');
     disp('finished training');
     reduced_test_bag = full(sparse_PCA(X_test_bag));
     % reduced_test_bag = reduced_test_bag(:,1:1000);
-    Y_hat = predict(SVM_model, reduced_test_bag);
+    Y_hat = str2double(predict(ensemble_model, reduced_test_bag));
 
 % Inputs:   X_test_bag     nx9995 bag of words features
 %           test_raw      nx1 cells containing all the raw tweets in text
