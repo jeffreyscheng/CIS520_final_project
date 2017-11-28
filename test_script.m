09980d827df3a8a44acb9c125c6922f94dbc06e5
Jeffrey Cheng <jeffch@wharton.upenn.edu>
2017-11-28 15:46:42 -0500

    liblinear sv

--- test_script.m
+++ test_script.m
@@ -1,7 +1,7 @@
 load('train.mat')
 load('validation.mat')
 load('vocabulary.mat')
-
+addpath('liblinear-2.11/windows/')
 t = cputime;
 [trainInd,valInd,testInd] = dividerand(18092,0.7,0.1,0.2);
 
@@ -10,49 +10,33 @@ trainingY = Y_train(trainInd,:);
 validationX = X_train_bag(valInd,:);
 validationY = Y_train(valInd,:);
 
-validationY(1:10,:);
-predictions(1:10,:);
-for k = [1, 2, 3, 5, 8, 10, 15, 20, 25]
-    k
-    predictions = predict_labels_jank(trainingX, trainingY, validationX, k);
-    score = performance_measure(predictions, validationY)
-end
+predictions = predict_labels_jank(trainingX, trainingY, validationX, 1);
+score = performance_measure(full(predictions), full(validationY))
+
+% for k = [1, 2, 3, 5, 8, 10, 15, 20, 25, 50, 100]
+%     k
+%     predictions = predict_labels_jank(training, Y_train, X_test_bag, k);
+%     score = performance_measure(full(predictions), full(Y_train))
+% end
 % e = cputime - t;
 
 function features = sparse_PCA(X, k)
-%     [~,~,PC] = svds(X,100);
-%     mu = mean(X);
-%     S = sparse(size(X,1),100);
-%     for i=1:size(X,1)
-%         S(i,:) = (X(i,:)-mu)*PC;
-%     end
-%     features = S;
-%     2
-
-% 0.1, 0.1, 1000 -> 1.6306
-% 0.3, 0.1, 500 -> 1.7690
     [U,S,V] = svds(X, k);
     features = U * S;
 end
 
 function [Y_hat] = predict_labels_jank(training_bag, training_labels, X_test_bag, k)
-    % nonsparse_training = full(training_bag);
-    % size(nonsparse_training)
-    % nonsparse_training(1:5)
-    reduced_train_bag = full(sparse_PCA(training_bag, k));
-    % reduced_train_bag = reduced_train_bag(:,1:1000)
-    ClassNames = {'1', '2', '3', '4', '5'};
-    training_labels = ClassNames(training_labels);
-    cost.ClassNames = ClassNames;
-    cost.ClassificationCosts = [0 3 1 2 3; 4 0 2 3 2; 1 2 0 2 1; 2 1 2 0 2; 2 2 2 1 0];
-    ensemble_model = fitcnb(reduced_train_bag,training_labels,'Cost',cost);
-%     ensemble_model = fitensemble(reduced_train_bag,training_labels,'AdaBoostM2',10,'tree','Cost',cost);
+%     reduced_train_bag = full(sparse_PCA(training_bag, k));
+    cost = [0 3 1 2 3; 4 0 2 3 2; 1 2 0 2 1; 2 1 2 0 2; 2 2 2 1 0];
+%     ensemble_model = fitcnb(training_bag,training_labels,'Cost',cost);
+    svm_model = train(full(training_labels), sparse(training_bag));
+%     ensemble_model = fitcnb(reduced_train_bag,training_labels,'Cost',cost);
+%     ensemble_model = fitensemble(reduced_train_bag,training_labels,'AdaBoostM2',500,'tree','Cost',cost);
 %     SVM_model = fitensemble(reduced_train_bag,training_labels,'AdaBoostM2',10,'tree');
     disp('finished training');
-    reduced_test_bag = full(sparse_PCA(X_test_bag, k));
-    % reduced_test_bag = reduced_test_bag(:,1:1000);
-    Y_hat = str2double(predict(ensemble_model, reduced_test_bag));
-
+%     reduced_test_bag = full(sparse_PCA(X_test_bag, k));
+%     Y_hat = predict(ensemble_model, reduced_test_bag);
+    Y_hat = predict(ones(size(X_test_bag,1),1), sparse(X_test_bag), svm_model)
 % Inputs:   X_test_bag     nx9995 bag of words features
 %           test_raw      nx1 cells containing all the raw tweets in text
 
